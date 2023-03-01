7e547c23f16693c390cd0bcb4a124e8fad9ca2e7
YAMIN <78724266+DeveloperYamin@users.noreply.github.com>
2023-03-01 17:49:09 +0600

    added scrollbar and back icon

--- src/EditProduct/EditProductHeader.js
+++ src/EditProduct/EditProductHeader.js
@@ -1,24 +1,34 @@
-import React from 'react'
-import './EditProductHeader.css';
-import iconImage from '../Assets/Vector.jpg';
+import React from "react";
+import "./EditProductHeader.css";
+import iconImage from "../Assets/Vector.jpg";
+import ArrowBack from "@material-ui/icons/ArrowBack";
+import { IconButton } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
 
 function Header() {
-    return (
-        <div className="edit__product__header__body">
-            <div className="edit__product__header__text">
-                <p>Edit Product</p>
-            </div>
-       
-            <div className="edit__product__header__box">
-                <div className="edit__product__header__box__icon">
-                   <img src={iconImage} alt=""/>
-                </div>
-                <div className="edit__product__header__box__text">
-                    <p>X<br/>Products</p>  
-                </div>   
-            </div>
+  const history = useHistory();
+  return (
+    <div className="edit__product__header__body">
+      <div className="edit__product__header__text">
+        <IconButton onClick={() => history.goBack()}>
+          <ArrowBack />
+        </IconButton>
+        <p style={{ marginLeft: "10px" }}>Edit Product</p>
+      </div>
+
+      <div className="edit__product__header__box">
+        <div className="edit__product__header__box__icon">
+          <img src={iconImage} alt="" />
         </div>
-    )
+        <div className="edit__product__header__box__text">
+          <p>
+            X<br />
+            Products
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
